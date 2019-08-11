e9bb28b52d04494041575c5f4293b6fafbcb3e22
Adrian Jablonski <adrian.jablonski19@gmail.com>
2019-08-11 16:27:31 -0500

    Built simple desktop electron app that shows system information

---
+++ Electron-Intro/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    // Create browser window
+    win = new BrowserWindow({ width: 800, height: 600, icon: __dirname + '/img/gear-icon.png', webPreferences: {
+        nodeIntegration: true
+    } });
+
+    // Load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+    }))
+
+    // Open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+// Run create window function
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') { // Checks if user is on a mac
+        app.quit();
+    }
+})
